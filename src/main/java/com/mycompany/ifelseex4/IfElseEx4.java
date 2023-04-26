2c1b752280ca68af702a15bbaa906c1fdea87800
Zaid <zaidrahim05@gmail.com>
2023-04-26 10:26:56 +0200

    initial commit

---
+++ src/main/java/com/mycompany/ifelseex4/IfElseEx4.java
@@ -0,0 +1,31 @@
+package com.mycompany.ifelseex4;
+import java.util.Scanner;
+
+public class IfElseEx4 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Enter a grade");
+        int n = scan.nextInt();
+        
+        if (n >= 90 && n <=100) {
+            System.out.println("A");
+        }
+        else if (n >= 80 && n <=90) {
+            System.out.println("B");  
+        }
+        else if (n >= 70 && n <= 80) {
+            System.out.println("C");
+        }
+        else if (n >= 60 && n <= 70) {
+            System.out.println("D");
+        }
+        else if (n >= 50 && n <= 60) {
+            System.out.println("E");
+        }
+        else {
+            System.out.println("F");
+        }
+    }
+}
